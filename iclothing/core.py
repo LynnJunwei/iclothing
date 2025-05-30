f162c172aa584cea047a5d248b2e138f97cc8477
LIN Junwei <junwei.lin@connect.polyu.hk>
2025-05-30 14:55:17 +0800

    revise input parameters

--- iclothing/core.py
+++ iclothing/core.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from typing import Literal
+from typing import Literal, Optional
 from types import FunctionType
 
-from iclothing.const import PWL_DICT_MALE, PWL_DICT_FEMALE, LOWER_LIMIT_DICT_MALE, LOWER_LIMIT_DICT_FEMALE, WIND_CORR_DICT
+from iclothing.const import PWL_DICT_GENERIC, PWL_DICT_STANDING, LOWER_LIMIT_DICT_GENERIC, LOWER_LIMIT_DICT_STANDING, WIND_CORR_DICT
 
 
 def get_vr(met: float, va: float = 0.1) -> float:
@@ -34,7 +34,7 @@ def get_vr(met: float, va: float = 0.1) -> float:
     .. math:: v_w = 0.25 \\times (met^2 + met - 2.4)
 
     Args:
-        met: A number of metabolic rate (in met).
+        met: A number of metabolic rate (in met = 58.15 W/m\ :sup:`2`).
         va: A number of absolute air velocity (in m/s).
 
     Returns:
@@ -72,8 +72,8 @@ def _get_icl_i(icl: list | np.ndarray | float,
 
 
 def get_icl_dict(icl: list | np.ndarray | float,
-                 sex: Literal["male", "female"],
-                 met: float = 1.0,
+                 posture: Literal["generic", "standing"] = "generic",
+                 met: Optional[float] = None,
                  va: float = 0.1) -> dict:
     """
     Get local clothing insulation for each body part.
@@ -82,29 +82,35 @@ def get_icl_dict(icl: list | np.ndarray | float,
 
     Args:
         icl: A number or a list of overall clothing insulation (in clo).
-        sex: Sex of human object. The value should be 'male' or 'female'. Default is 'male'.
-        met: A number of Metabolic rate of human object (in met). Default is 1.0 met.
-        va: A number of absolute air velocity (in m/s). Default is 0.1 m/s.
+        posture: Posture of human object. The value should be 'generic' or 'standing'. Generic posture is based on
+            data from both sitting and standing postures, while standing posture is based on data from standing posture
+            only. Default is 'generic'.
+        met: A number of Metabolic rate of human object (in met). If input, it will be used to calculated relative
+            air velocity (in m/s). Default is None.
+        va: A number of absolute air velocity (in m/s). If no met input, relative air velocity equals absolute
+            air velocity. Default is 0.1 m/s, which represents static indoors.
 
     Returns:
         A dictionary of local clothing insulation for each body part.
 
     Examples:
         >>> icl = 0.3
-        >>> icli = get_icl_dict(icl=icl, sex="male", met=1)
-        >>> print(icli)
-        {'Head': 0.13, 'Neck': 0.0, 'Chest': 0.35, 'Back': 0.27, 'Pelvis': 0.91, 'LShoulder': 0.0, 'LArm': 0.0, 'LHand': 0.0, 'RShoulder': 0.0, 'RArm': 0.0, 'RHand': 0.0, 'LThigh': 0.48, 'LLeg': 0.043, 'LFoot': 1.342, 'RThigh': 0.48, 'RLeg': 0.043, 'RFoot': 1.342}
+        >>> icli = get_icl_dict(icl=icl, posture="generic")
+        >>> icli
+        {'Head': 0.13, 'Neck': 0.0, 'Chest': 0.59, 'Back': 0.648, 'Pelvis': 1.114, 'LShoulder': 0.207, 'LArm': 0.0,
+        'LHand': 0.0, 'RShoulder': 0.207, 'RArm': 0.0, 'RHand': 0.0, 'LThigh': 0.618, 'LLeg': 0.054, 'LFoot': 0.425,
+        'RThigh': 0.618, 'RLeg': 0.054, 'RFoot': 0.425}
     """
-    if sex == 'male':
-        icl_dict = {body_name: _get_icl_i(icl, pwl, LOWER_LIMIT_DICT_MALE[body_name])
-                    for body_name, pwl in PWL_DICT_MALE.items()}
-    elif sex == 'female':
-        icl_dict = {body_name: _get_icl_i(icl, pwl, LOWER_LIMIT_DICT_FEMALE[body_name])
-                    for body_name, pwl in PWL_DICT_FEMALE.items()}
+    if posture == 'generic':
+        icl_dict = {body_name: _get_icl_i(icl, pwl, LOWER_LIMIT_DICT_GENERIC[body_name])
+                    for body_name, pwl in PWL_DICT_GENERIC.items()}
+    elif posture == 'standing':
+        icl_dict = {body_name: _get_icl_i(icl, pwl, LOWER_LIMIT_DICT_STANDING[body_name])
+                    for body_name, pwl in PWL_DICT_STANDING.items()}
     else:
         raise ValueError
 
-    vr = get_vr(met, va)
+    vr = get_vr(met, va) if met is not None else va
     if vr > 0.2:
         icl_dict = {body_name: (icl_i * WIND_CORR_DICT[body_name](vr))
                     for body_name, icl_i in icl_dict.items()}
@@ -113,4 +119,6 @@ def get_icl_dict(icl: list | np.ndarray | float,
 
 
 if __name__ == '__main__':
-    pass
+    icl = 0.3
+    icli = get_icl_dict(icl=icl, posture="generic")
+    print(icli)
